de753dd4dc033bf09f7ee45019868633d9057697
iot-root <darthponeh@gmail.com>
2024-03-24 18:04:16 -0400

    fix: add i2c address not found exception for ina219 pump curr monitor
     Changes to be committed:
    	modified:   app/sensors/pump/pump_power.py

--- app/sensors/pump/pump_power.py
+++ app/sensors/pump/pump_power.py
@@ -1,26 +1,38 @@
 # coding=utf-8
+import smbus
 from ina219 import INA219
 from ina219 import DeviceRangeError
+import time
 
 SHUNT_OHMS = 0.08
 
-def fetch_ina219_data():
+def is_ina219_present(address):
     try:
-        ina = INA219(SHUNT_OHMS)
-        ina.configure()
-            
-        data = {
-            'BusVoltage': ina.voltage(),
-            'BusCurrent': None,
-            'Power': None,
-            'ShuntVoltage': ina.shunt_voltage(),
-        }
-    
-    
-        data['Bus Current'] = ina.current()
-        data['Power'] = ina.power()
-    except DeviceRangeError as e:
-        data['error'] = str(e)
+        bus = smbus.SMBus(1)  # Use SMBus(0) for older versions of Raspberry Pi
+        bus.read_byte_data(address, 0)  # Try to read a byte from the specified address
+        return True  # If no exception is raised, the device is present
+    except Exception:
+        return False
+
+def fetch_ina219_data():
+    data = {}
+    if is_ina219_present(0x40):  # Check if the INA219 is present at address 0x40
+        try:
+            ina = INA219(SHUNT_OHMS, address=0x40)  # Specify the INA219's address
+            ina.configure()
+            time.sleep(1)
+            data = {
+                'BusVoltage': ina.voltage(),
+                'BusCurrent': None,
+                'Power': None,
+                'ShuntVoltage': ina.shunt_voltage(),
+            }
+            data['Bus Current'] = ina.current()
+            data['Power'] = ina.power()
+        except DeviceRangeError as e:
+            data['error'] = str(e)
+    else:
+        data['error'] = "INA219 not found at address 0x40"
     
     return data
 
@@ -33,13 +45,7 @@ if __name__ == '__main__':
         print("INA219 Sensor Data:")
         for key, value in sensor_data.items():
             print(f"{key}, value={value}")
-
-        #  print(f"temperature, value={temperature:.2f}")
     except Exception as e:
         print(f"Error: {e}")
     except KeyboardInterrupt:
         print("Script interrupted.")
-
-
-
-
