88886a4e9e11427a97bcac67f0cbb39aeed7cbcd
iot-root <darthponeh@gmail.com>
2024-03-24 17:52:38 -0400

    fix: fix paths for data collector bin/get-sensor-data.sh

--- bin/get-sensor-data.sh
+++ bin/get-sensor-data.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+pwd=$(dirname $(readlink -f $0))
+wkdir=$(realpath $pwd/..)
 
+py="$wkdir/venv/bin/python"
 # Define the log file location
-LOG_FILE="/var/log/gardyn/data.log"
+LOG_FILE="/var/log/gardyn-data.log"
+
+sudo touch $LOG_FILE
 #echo "test" >> $LOG_FILE
 
 # Get the current timestamp
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Execute Python scripts and capture their output
-humidity=$(/usr/bin/python3 /home/gardyn/projects/gardyn-of-eden/app/sensors/humidity/humidity.py)
-temperature=$(/usr/bin/python3 /home/gardyn/projects/gardyn-of-eden/app/sensors/temperature/temperature.py)
-distance=$(/usr/bin/python3 /home/gardyn/projects/gardyn-of-eden/app/sensors/distance/distance.py)
+humidity=$($py $wkdir/app/sensors/humidity/humidity.py)
+temperature=$($py $wkdir/app/sensors/temperature/temperature.py)
+distance=$($py $wkdir/app/sensors/distance/distance.py)
 
 # Append the results to the log file
 echo "$timestamp, $humidity" >> $LOG_FILE
