18733869c3236c7f27e49b496da65a5645ac9774
iot-root <darthponeh@gmail.com>
2024-03-24 23:46:49 -0400

    refactor: use dotenv for config.py and create .env template .env-dist  Changes to be committed: 	new file:   .env-dist 	modified:   config.py 	modified:   requirements.txt

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # MQTT configurations
-BROKER = "localhost"
-PORT = 1883
-KEEP_ALIVE_INTERVAL = 5
+BROKER = os.getenv("MQTT_BROKER", "localhost")
+PORT = os.getenv("MQTT_PORT", "1883")
+KEEP_ALIVE_INTERVAL = os.getenv("MQTT_KEEPALIVE_INTERVAL", "60")
 
 # Topic configurations
-BASE_TOPIC = "eden"
+BASE_TOPIC = os.getenv("MQTT_BASETOPIC", "eden")
 
-USERNAME = 'gardyn'
-PASSWORD = 'somepass'
+USERNAME = os.getenv("MQTT_USERNAME", "gardyn")
+PASSWORD = os.getenv("MQTT_PASSWORD", "somepassword")
