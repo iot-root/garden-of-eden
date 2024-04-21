8abcd915f60c19623aa0f645ee0ebf552f037281
Peter Correa <pmcorrea@me.com>
2024-04-21 19:06:40 -0400

    refactor: updated endpoint for consistency

--- app/sensors/distance/routes.py
+++ app/sensors/distance/routes.py
@@ -4,7 +4,7 @@ from .distance import Distance as DistanceControl
 distance_blueprint = Blueprint('distance', __name__)
 distance_control = DistanceControl()
 
-@distance_blueprint.route('/measure', methods=['GET'])
+@distance_blueprint.route('', methods=['GET'])
 def get_distance():
     distance_value = distance_control.measure_once()
     return jsonify(distance=distance_value), 200
