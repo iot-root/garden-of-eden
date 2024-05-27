1125e047481527a88c83188d4d7ef31bbc630535
Peter Correa <pmcorrea@me.com>
2024-05-27 04:04:36 -0400

    updated log script path

--- api/app/log/routes.py
+++ api/app/log/routes.py
@@ -9,7 +9,7 @@ log_blueprint = Blueprint('log', __name__)
 def capture_logs():
     try:
         # Define the script path
-        script_path = os.path.join(os.getcwd(), '../../bin/api/log-sensor-data.sh')
+        script_path = os.path.join(os.getcwd(), 'bin/api/log-sensor-data.sh')
         print(script_path)
         # Execute the script
         result = subprocess.run([script_path], capture_output=True, text=True, shell=True)
@@ -18,6 +18,6 @@ def capture_logs():
         if result.returncode != 0:
             return jsonify({"error": result.stderr}), 500
 
-        return jsonify({"output": result.stdout})
+        return jsonify({"msg": "logs recorded, please refresh"})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
