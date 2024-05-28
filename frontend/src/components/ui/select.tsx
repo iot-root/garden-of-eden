55bfaeda4dcc62ed76abbb518bb63d4e98e846cc
Peter Correa <pmcorrea@me.com>
2024-05-27 22:29:24 -0400

    removed dup classes

--- frontend/src/components/ui/select.tsx
+++ frontend/src/components/ui/select.tsx
@@ -27,7 +27,7 @@ export const SelectTrigger = <T extends ValidComponent = 'button'>(
   return (
     <SelectPrimitive.Trigger
       class={cn(
-        'flex h-9 w-full items-center justify-between rounded-md border border-slate-200 border-slate-200 bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-white transition-shadow placeholder:text-slate-500 focus:outline-none focus-visible:ring-[1.5px] focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:border-slate-800 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300',
+        'flex h-9 w-full items-center justify-between rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-white transition-shadow placeholder:text-slate-500 focus:outline-none focus-visible:ring-[1.5px] focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300',
         local.class
       )}
       {...rest}
--- frontend/src/components/ui/textfield.tsx
+++ frontend/src/components/ui/textfield.tsx
@@ -110,7 +110,7 @@ export const TextField = <T extends ValidComponent = 'input'>(
   return (
     <TextFieldPrimitive.Input
       class={cn(
-        'flex h-9 w-full rounded-md border border-slate-200 border-slate-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-shadow file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-[1.5px] focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300',
+        'flex h-9 w-full rounded-md border border-slate-200bg-transparent px-3 py-1 text-sm shadow-sm transition-shadow file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-[1.5px] focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300',
         local.class
       )}
       {...rest}
