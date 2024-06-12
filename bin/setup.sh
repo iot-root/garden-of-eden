2fd9e617efafd9f17d00048e0917b1740425cb58
iot-root <140926906+iot-root@users.noreply.github.com>
2024-06-12 15:03:17 -0400

    refactor: update setup.sh
    Improve DHT20 and AM2320 humidity temp detection.

--- bin/setup.sh
+++ bin/setup.sh
@@ -29,7 +29,8 @@ function log_info {
 
 function log {
     if [ "$VERBOSE" == "true" ]; then
-        echo "$@"
+        #echo "$@"
+        echo -e "[${GRY}INFO${RST}]: ${LGY}$*${RST}" >&2
     fi
 }
 
@@ -89,15 +90,34 @@ function setup_python_env {
     deactivate
 }
 
-# Enable I2C if not enabled
-function enable_i2c {
-    local i2c_status=$(sudo raspi-config nonint get_i2c)
-    if [ "$i2c_status" -eq 1 ]; then
-        log_info "I2C is not enabled. Enabling now..."
-        sudo raspi-config nonint do_i2c 1
-        log_pass "I2C enabled."
+# Function to enable I2C in /boot/config.txt and configure I2C
+# See https://github.com/fivdi/i2c-bus/blob/master/doc/raspberry-pi-i2c.md
+function enable_i2c_config_txt() {
+    local config_file="/boot/config.txt"
+    local param="dtparam=i2c_arm=on"
+    log "Enabling I2C in $config_file..."
+
+    # Remove any existing line with dtparam=i2c_arm and add the correct one
+    sudo sed -i "/^#*dtparam=i2c_arm/c\\$param" "$config_file"
+    log "I2C has been enabled in $config_file."
+
+    # Enable I2C interface
+    sudo raspi-config nonint do_i2c 0
+
+    # Check if i2c-dev is already in /etc/modules
+    log "Configuring I2C modules..."
+    sudo sed -i '/^#*i2c-dev/d' /etc/modules
+    echo "i2c-dev" | sudo tee -a /etc/modules > /dev/null
+}
+
+
+# Function to load I2C module immediately
+function  load_i2c_module() {
+    if lsmod | grep -q i2c_dev; then
+        log_pass "I2C module is already loaded."
     else
-        log_info "I2C is already enabled."
+        log_info "Loading I2C module..."
+        sudo modprobe i2c-dev
     fi
 }
 
@@ -108,6 +128,17 @@ function check_i2c_detection {
         log_pass "i2cdetect executed successfully."
     else
         log_error "i2cdetect encountered an error."
+    fi
+}
+
+# Function to restart the system if necessary
+function  restart_system() {
+    echo "The system needs to be restarted to apply the changes. Do you want to restart now? (y/n)"
+    read -r answer
+    if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+        sudo reboot
+    else
+        echo "Please remember to reboot your system later to apply the changes."
     fi
 }
 
@@ -141,28 +172,39 @@ function add_user_to_groups {
 # If a gardyn model 1.0 or 2.0, AM2320 will not appear on i2cdetect without wakeup sequeence.
 # so we assume it is an AM2320 unless we find a DHT20
 function add_sensor_type_to_env {
+    log_info "Setting temp&humidity SENSOR_TYPE in .env"
     if ! grep -q "SENSOR_TYPE=" $INSTALL_DIR/.env; then
         local sensor_type="AM2320"
         if i2cdetect -y 1 | grep -q "38"; then
             sensor_type="DHT20"
         fi
+        log_info "Setting SENSOR_TYPE=$sensor_type in .env"
         echo "SENSOR_TYPE=$sensor_type" >> $INSTALL_DIR/.env
     fi
 }
 
-# Check for the presence of an I2C device
 function check_device {
     local device_name=$1
     local addr_hex=$2
+    local max_retries=${3:-1}
+    local retry_interval=${4:-0.1}
     local addr_dec=$((16#$addr_hex))
     local row=$((addr_dec / 16))
     local col=$((addr_dec % 16))
-    
-    if sudo i2cdetect -y 1 | awk -v row="$row" -v col="$col" 'NR == row + 2 && $((col + 2)) != "--" {print $((col + 2))}' | grep -q "$addr_hex"; then
-        log_pass "$device_name detected at address 0x$addr_hex."
-    else
-        log_error "$device_name not detected at address 0x$addr_hex."
-    fi
+
+    log_info "Checking for $device_name at address 0x$addr_hex..."
+
+    for ((i=1; i<=max_retries; i++))
+    do
+        if sudo i2cdetect -y 1 | awk -v row="$row" -v col="$col" 'NR == row + 2 && $((col + 2)) != "--" {print $((col + 2))}' | grep -q "$addr_hex"; then
+            log_pass "$device_name detected at address 0x$addr_hex."
+            return 0
+        fi
+        sleep $retry_interval
+        log_info "Attempt $i: $device_name not detected, retrying..."
+    done
+
+    log_error "$device_name not detected after $max_retries attempts."
 }
 
 # Check for I2C sensors
@@ -170,10 +212,11 @@ function check_i2c_sensors {
     log_info "Checking for I2C sensors"
     check_device "PCT2075" "48"
     check_device "INA219" "40"
-    log_info "Note: the DHT20 temperature & humidity sensor should only appear for Gardyn 3.0 and newer"
+    log "Note: the DHT20 temperature & humidity sensor should only appear for Gardyn 3.0 and newer"
     check_device "DHT20" "38"
-    log_info "Note: the AM2320 temperature & humidity sensor should only appear for Gardyn 1.0 and 2.0"
-    check_device "AM2320" "5c"
+    # note different method to detect AM2320
+    log "Note: the AM2320 temperature & humidity sensor should only appear for Gardyn 1.0 and 2.0"
+    check_device "AM2320" "5c" "10" "0.002"
 }
 
 # Ensure pigpio daemon runs after system reboots
@@ -215,12 +258,14 @@ cd $INSTALL_DIR
 
 install_packages
 setup_python_env
-enable_i2c
+
+enable_i2c_config_txt
+load_i2c_module
 check_i2c_detection
 ensure_env_file
 add_user_to_groups
-add_sensor_type_to_env
 check_i2c_sensors
+add_sensor_type_to_env
 
 #Note: pigpiod will be started by mqtt.service
 #enable_pigpiod_service
